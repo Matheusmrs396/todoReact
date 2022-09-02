e6169049a58933dbb07e6c8a7a3d4c37a915177b
unknown <matheusmrs396@gmail.com>
2022-09-02 18:35:28 -0300

    Iniciando projeto TODO

---
+++ src/App.js
@@ -0,0 +1,97 @@
+import './App.css';
+
+import {useState, useEffect} from "react";
+import {BsTrash, BsBookmarkCheck, BsBookmarkCheckFill} from "react-icons/bs";
+
+const API = "http://localhost:5000";
+
+function App() {
+  const [title, setTitle] = useState("");
+  const [time, setTime] = useState("");
+  const [todos, setTodos] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() =>{
+
+    const loadData = async()=>{
+      setLoading(true);
+
+      const res = await fetch(API + "/todos").then((res)=> res.json).then((data)=> data);
+
+      setLoading(false);
+
+      setTodos(res);
+    };
+
+    loadData();
+
+  },[])
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+
+  const todo = {
+    id: Math.random(),
+    title,
+    time,
+    done:false,
+  };
+  await fetch(API + "/todos",{
+    method: "Post",
+    body: JSON.stringify(todo),
+    headers: {
+      "Content-type": "application/json",
+    },
+  });
+//TODO analisar o porque de map não estar funcionando
+  setTitle("");
+  setTime("");
+}
+  return (
+    <div className="App">
+      <div className="todo-header">
+        <h1>React Todo</h1>
+      </div>
+      <div className="form-todo">
+        <h2> Insira a sua tarefa </h2>
+        <form onSubmit={handleSubmit}>
+          <div className="form-control">
+            <label htmlFor="title">Título da tarefa</label>
+            <input
+            type="text" 
+            name="title" 
+            placeholder="Título da tarefa" 
+            onChange={(e)=> setTitle(e.target.value)}
+            value={title || ""}
+            required 
+            />
+          </div>
+          <div className="form-control">
+            <label htmlFor="time">Quantas horas irá durar</label>
+            <input
+            type="text" 
+            name="time" 
+            placeholder="tempo de duração em horas" 
+            onChange={(e)=> setTime(e.target.value)}
+            value={time || ""}
+            required 
+            />
+          </div>
+          <input type="submit" value="cadastrar"/>
+        </form>
+      </div>
+      <div className="list-todo">
+        <h2> Listas de tarefas:</h2>
+        {todos.length === 0 && <p>Não existem tarefas</p>}
+
+        {todos.map((todo)=>(
+          <div className="todo" key={todo.id}>
+            <p>{todo.title}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
